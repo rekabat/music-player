0e3caa3599b821e9b10f8fac2bffb011c939c1d8
Troy Baker <rekabat@gmail.com>
2017-03-19 19:18:15 -0400

    A LOT of styling. Attempt at an image toggle button and to return to the same point in list when going back.

--- TygerAudio/app/src/main/java/com/audio/tyger/tygeraudio/BrowserActivity.java
+++ TygerAudio/app/src/main/java/com/audio/tyger/tygeraudio/BrowserActivity.java
@@ -1,6 +1,8 @@
 package com.audio.tyger.tygeraudio;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,16 +13,33 @@ import android.widget.ListView;
 import android.widget.AdapterView;
 import android.widget.TextView;
 
+import java.util.List;
+import java.util.Stack;
+
+
+//$base03:    #002b36; Background
+//$blue:      #268bd2; Dir font
+//$base00:    #657b83; File font
+
 
 public class BrowserActivity extends AppCompatActivity { //implements OnItemClickListener  {
 
     private ListView filelv;
     final private String initialDir = "/";
 
+    private Stack<Parcelable> scrollPositions = new Stack();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browser);
+
+        // set color scheme
+//        int background = 0x002b36;
+//        int fontDir    = 0x268bd2;
+//        int fontFile   = 0x657b83;
+////        setColorTheme(background, fontDir, fontFile);
+//        setColorTheme(Color.parseColor("#002b36"), fontDir, fontFile);
 
         // get directory listView
         filelv = (ListView) findViewById(R.id.fileList);
@@ -29,27 +48,35 @@ public class BrowserActivity extends AppCompatActivity { //implements OnItemClic
         // get the titlebar textview
         final TextView tvCurDir = (TextView) findViewById(R.id.textCurDir);
 
+        // set initial directory to be displayed
+        tvCurDir.setText(initialDir);
+
         // set the onClick listener
         filelv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                // add current state to the backstack
-//                getSupportFragmentManager().beginTransaction()
-//                        .add(details)
-//                        .addToBackStack()
-//                        .commit();
-                // get new directory
-                String s = (String) parent.getItemAtPosition(position);
-                // set new directory
+                // get adapter
                 DirectoryAdapter da = (DirectoryAdapter) parent.getAdapter();
-                da.selectDirectory(s, tvCurDir);
-                // scroll to top
-                parent.setSelection(0);
+                // get clicked item
+                // TODO: no reason to get string, just pass along the position
+                String s = (String) parent.getItemAtPosition(position);
+
+                if (da.isDirectory(position)) {
+                    // save the old position for going back
+                    scrollPositions.push(filelv.onSaveInstanceState());
+                    Log.d("Scroll pos click", Integer.toString(parent.getScrollY()));
+
+                    // set new directory
+                    da.selectDirectory(s, tvCurDir);
+                    // scroll to top
+                    parent.setSelection(0);
+
+                } else { // it's a file
+                    // open file
+                }
             }
         });
 
-        // set initial directory to be displayed
-        tvCurDir.setText(initialDir);
     }
 
     @Override
@@ -70,7 +97,12 @@ public class BrowserActivity extends AppCompatActivity { //implements OnItemClic
             System.exit(1);
         }
         // scroll to top
-        filelv.setSelection(0);
+//        filelv.setSelection(0);
+
+        // get directory listview
+        filelv.onRestoreInstanceState(scrollPositions.peek());
+//        Log.d("Scroll pos back", Integer.toString(scrollPositions.peek()));
+        scrollPositions.pop();
     }
 
 
@@ -83,6 +115,19 @@ public class BrowserActivity extends AppCompatActivity { //implements OnItemClic
 //        // Or / And
 //        intent.putExtra("id", id);
 //        startActivity(intent);
+//    }
+
+
+
+    private void setColorTheme(int background, int fontDir, int fontFile) {
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(background);
+//        view.set
+
+    }
+
+//    private int rgbToInt(int r, int g, int b) {
+//        return ((r&0x0ff)<<16)|((g&0x0ff)<<8)|(b&0x0ff);
 //    }
 
 }
--- TygerAudio/app/src/main/java/com/audio/tyger/tygeraudio/DirectoryAdapter.java
+++ TygerAudio/app/src/main/java/com/audio/tyger/tygeraudio/DirectoryAdapter.java
@@ -1,5 +1,7 @@
 package com.audio.tyger.tygeraudio;
 
+import android.graphics.Typeface;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,11 +23,71 @@ public class DirectoryAdapter extends BaseAdapter {
 
     private String currentPath;
     private ArrayList<String> dirs;
+    private ArrayList<String> files;
 
     public DirectoryAdapter(String path) {
         currentPath = path;
         dirs = getDirectories(path);
+        files = getFiles(path);
     }
+
+    @Override
+    public int getCount() {
+        return dirs.size() + files.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        if (position < dirs.size())
+            return dirs.get(position);
+        else {
+            position -= dirs.size();
+            return files.get(position);
+        }
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // get view format from xml
+        if (convertView == null) {
+            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+            convertView = inflater.inflate(R.layout.listview_item_directory, parent, false);
+        }
+
+        // get textview
+        TextView tv = (TextView) convertView.findViewById(R.id.lv_item_dir_text);
+
+        // set the display string
+        final String s = (String) getItem(position);
+        tv.setText(s);
+
+        // set the font weight (bold if dir, normal if file)
+        if (isDirectory(position))
+            tv.setTextColor(ContextCompat.getColor(convertView.getContext(), R.color.colorFontPrimary));
+        else
+            tv.setTextColor(ContextCompat.getColor(convertView.getContext(), R.color.colorFontSecondary));
+        //            tv.setTypeface(null, Typeface.BOLD);\
+
+
+//        convertView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                view.get
+//                Log.d("click", s);
+////                Toast.makeText(parent.getContext(), "view clicked: " + dataModel.getOtherData(), Toast.LENGTH_SHORT).show();
+//            }
+//        });
+
+        return convertView;
+    }
+
+
+
 
     public void selectDirectory(String dir, TextView displayDir) {
         updatePath(currentPath+dir, displayDir);
@@ -41,47 +103,13 @@ public class DirectoryAdapter extends BaseAdapter {
     public void updatePath(String path, TextView displayDir) {
         currentPath = path;
         dirs = getDirectories(currentPath);
+        files = getFiles(currentPath);
         updateDisplayedDirectory(displayDir);
         notifyDataSetChanged();
     }
 
-    @Override
-    public int getCount() {
-        return dirs.size();
-    }
-
-    @Override
-    public Object getItem(int position) {
-        return dirs.get(position);
-    }
-
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
-
-    @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        if (convertView == null) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            convertView = inflater.inflate(R.layout.listview_item_directory, parent, false);
-        }
-
-        TextView tv = (TextView) convertView.findViewById(R.id.lv_item_dir_text);
-
-        final String s = dirs.get(position);
-        tv.setText(s);
-
-//        convertView.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                view.get
-//                Log.d("click", s);
-////                Toast.makeText(parent.getContext(), "view clicked: " + dataModel.getOtherData(), Toast.LENGTH_SHORT).show();
-//            }
-//        });
-
-        return convertView;
+    public boolean isDirectory(int position) {
+        return (position < dirs.size());
     }
 
 
@@ -116,7 +144,6 @@ public class DirectoryAdapter extends BaseAdapter {
             // get all directories
             for (File inFile : files) {
                 if (inFile.isDirectory()) {
-                    // is directory
                     String s = inFile.toString();
                     s = s.substring(path.length());
                     ret.add(s);
@@ -129,6 +156,32 @@ public class DirectoryAdapter extends BaseAdapter {
             // append "/" to all directories. Must be done afterwards for alphabetizing purposes
             for (ListIterator s = ret.listIterator(); s.hasNext(); )
                 s.set(s.next() + "/");
+        }
+
+        return ret;
+    }
+
+    private ArrayList<String> getFiles(String path) {
+        ArrayList<String> ret = new ArrayList<String>();
+
+        // get file list
+        File f = new File(path);
+        File[] files = f.listFiles();
+
+        if (files != null) {
+
+            // get all files
+            for (File inFile : files) {
+                if (inFile.isFile()) {
+                    String s = inFile.toString();
+                    s = s.substring(path.length());
+                    ret.add(s);
+                }
+            }
+
+            // alphabetize
+            java.util.Collections.sort(ret, java.text.Collator.getInstance());
+
         }
 
         return ret;
